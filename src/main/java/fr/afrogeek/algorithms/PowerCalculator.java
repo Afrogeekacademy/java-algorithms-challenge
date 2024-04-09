f0e01a65bd0f72a0d6ebd68fd688978a0e795e81
PradermN <PradermN@chromium.org>
2024-04-09 21:51:03 +0200

    #20-ajout-de-nouveaux-tests-unitaires

--- src/main/java/fr/afrogeek/algorithms/PowerCalculator.java
+++ src/main/java/fr/afrogeek/algorithms/PowerCalculator.java
@@ -3,7 +3,18 @@ package fr.afrogeek.algorithms;
 import java.lang.Math;
 public class PowerCalculator {
 
-    public static double calculatePower(double x, int n){
-        return Math.pow(x, n);
+    public static double calculatePower(double x, int n) {
+        if (x == 0) {
+            if (n < 0) {
+                throw new IllegalArgumentException("Unendlich!!!");
+            } else if (n == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return Math.pow(x, n);
+        }
     }
 }
+
