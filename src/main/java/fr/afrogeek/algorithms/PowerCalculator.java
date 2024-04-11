50577af42ce3bd72be4b162271524a47ff0bdea2
PradermN <PradermN@chromium.org>
2024-04-11 13:23:36 +0200

    #20-implementation-sasn-mathPow

--- src/main/java/fr/afrogeek/algorithms/PowerCalculator.java
+++ src/main/java/fr/afrogeek/algorithms/PowerCalculator.java
@@ -1,20 +1,28 @@
 package fr.afrogeek.algorithms;
 
-import java.lang.Math;
 public class PowerCalculator {
 
     public static double calculatePower(double x, int n) {
-        if (x == 0) {
-            if (n < 0) {
-                throw new IllegalArgumentException("Unendlich!!!");
-            } else if (n == 0) {
-                return 1;
-            } else {
-                return 0;
+        if (n < 0) {
+            if (x == 0) {
+                throw new IllegalArgumentException("Undefiniert!!!");
             }
+            return 1 / calculatePowerPositive(x, -n);
         } else {
-            return Math.pow(x, n);
+            return calculatePowerPositive(x, n);
         }
+    }
+
+    private static double calculatePowerPositive(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double result = 1;
+
+        for (int i = n; i > 0; i--) {
+            result *= x;
+        }
+        return result;
     }
 }
 
