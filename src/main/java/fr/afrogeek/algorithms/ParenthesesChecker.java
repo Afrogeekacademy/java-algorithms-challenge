b50053bd6b11fca827701baf59e2681b03ed3454
aams01 <130369166+auguste2003@users.noreply.github.com>
2024-04-11 23:50:19 +0200

    Implementation du issue 26 sur les parentheses

--- src/main/java/fr/afrogeek/algorithms/ParenthesesChecker.java
+++ src/main/java/fr/afrogeek/algorithms/ParenthesesChecker.java
@@ -9,14 +9,14 @@ public class ParenthesesChecker {
 
         for (char c :  str.toCharArray()) {
             if (c == '(') { // Si on trouvre une parenthese , on ajoute dans le stack
-                stack.push(c);
+                stack.push(c); // On ajoute au stack
             } else if (c == ')') { // Si on trouve uniquement une parenthese fermente mais pas ouvrante , on vérifie si le stack est vide
-                if (stack.isEmpty()) {
+                if (stack.isEmpty()) { // On vérifie si le stack est vide
                     return false; // Aucune parenthèse ouvrante pour cette parenthèse fermante
                 }
-                stack.pop();
+                stack.pop(); // On rétire la parenthese ouvrante .
             }
         }
-        return stack.isEmpty(); // Vrai si toutes les parenthèses sont appariées
+        return stack.isEmpty(); // L'ajout et le retrait conduit au fait que le stack soit null
     }
 }
