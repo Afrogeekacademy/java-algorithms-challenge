8e6d7b62e0242dfe0b9f3ccb7a642b7f6a142359
aams01 <130369166+auguste2003@users.noreply.github.com>
2024-04-27 10:40:39 +0200

    supprission du fichier miss

--- src/test/java/fr/afrogeek/algorithms/ParenthesesCheckerTest.java
+++ src/test/java/fr/afrogeek/algorithms/ParenthesesCheckerTest.java
@@ -20,27 +20,27 @@ public class ParenthesesCheckerTest {
 
         @Test
         public void testBalancedParentheses() { // Texte le cas oú les parentheses balancent
-            assertTrue(App.checkBalancedParentheses("()"));
-            assertTrue(App.checkBalancedParentheses("(())"));
-            assertTrue(App.checkBalancedParentheses("(()())"));
+            assertTrue(ParenthesesChecker.checkBalancedParentheses("()"));
+            assertTrue(ParenthesesChecker.checkBalancedParentheses("(())"));
+            assertTrue(ParenthesesChecker.checkBalancedParentheses("(()())"));
         }
 
         @Test
         public void testUnbalancedParentheses() { // Teste les cas oú le nombre les impair
-            assertFalse(App.checkBalancedParentheses("("));
-            assertFalse(App.checkBalancedParentheses(")"));
-            assertFalse(App.checkBalancedParentheses("(()"));
-            assertFalse(App.checkBalancedParentheses("())"));
+            assertFalse(ParenthesesChecker.checkBalancedParentheses("("));
+            assertFalse(ParenthesesChecker.checkBalancedParentheses(")"));
+            assertFalse(ParenthesesChecker.checkBalancedParentheses("(()"));
+            assertFalse(ParenthesesChecker.checkBalancedParentheses("())"));
         }
 
         @Test
         public void testEmptyString() { // teste le cas oú le string est vide
-            assertTrue(App.checkBalancedParentheses(""));
+            assertTrue(ParenthesesChecker.checkBalancedParentheses(""));
         }
 
         @Test
         public void testStringWithoutParentheses() { // Teste le cas ou le string ne contient pas de parenthese
-            assertTrue(App.checkBalancedParentheses("Hello World!"));
+            assertTrue(ParenthesesChecker.checkBalancedParentheses("Hello World!"));
         }
 
     }
