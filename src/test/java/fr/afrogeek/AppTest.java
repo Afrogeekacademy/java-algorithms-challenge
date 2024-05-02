606417e82a276caaef5553950ea16b695e533600
aams01 <130369166+auguste2003@users.noreply.github.com>
2024-05-02 18:45:02 +0200

    Parenthesechecker

--- src/test/java/fr/afrogeek/AppTest.java
+++ src/test/java/fr/afrogeek/AppTest.java
@@ -1,47 +1,7 @@
 package fr.afrogeek;
 
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @ Test
-    public void testApp() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testBalancedParentheses() { // Texte le cas oú les parentheses balancent
-        assertTrue(App.checkBalancedParentheses("()"));
-        assertTrue(App.checkBalancedParentheses("(())"));
-        assertTrue(App.checkBalancedParentheses("(()())"));
-    }
-
-    @Test
-    public void testUnbalancedParentheses() { // Teste les cas oú le nombre les impair
-        assertFalse(App.checkBalancedParentheses("("));
-        assertFalse(App.checkBalancedParentheses(")"));
-        assertFalse(App.checkBalancedParentheses("(()"));
-        assertFalse(App.checkBalancedParentheses("())"));
-    }
-
-    @Test
-    public void testEmptyString() { // teste le cas oú le string est vide
-        assertTrue(App.checkBalancedParentheses(""));
-    }
-
-    @Test
-    public void testStringWithoutParentheses() { // Teste le cas ou le string ne contient pas de parenthese
-        assertTrue(App.checkBalancedParentheses("Hello, World!"));
-    }
 
 }
