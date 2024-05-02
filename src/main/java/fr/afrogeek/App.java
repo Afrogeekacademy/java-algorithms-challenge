79f1e7b3c74da5591ca7f2d9cb437440e26db4a9
aams01 <130369166+auguste2003@users.noreply.github.com>
2024-05-02 18:47:43 +0200

    Parenthesechecker

--- src/main/java/fr/afrogeek/App.java
+++ src/main/java/fr/afrogeek/App.java
@@ -1,10 +1,13 @@
 package fr.afrogeek;
-import java.util.Stack;
-import java.util.Random;
 
-
-public class App {
-    public static void main(String[] args) {
-
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
