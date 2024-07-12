1b5352b5e9fc4abb41ad03d3e5d02acf1c7960a7
eugenie Bastiane Djamen <bastiane@MacBook-Pro-von-eugenie.local>
2024-07-12 12:07:17 +0200

    #50-Modified the MainMax class and the test class

--- java-algorithms-challenge/src/main/java/fr/afrogeek/algorithms/MaxOfTwo.java
+++ java-algorithms-challenge/src/main/java/fr/afrogeek/algorithms/MaxOfTwo.java
@@ -1,7 +1,37 @@
 package fr.afrogeek.algorithms;
 
 public class MaxOfTwo {
-    public static int maxOfTwoNumbers(int a, int b){
-        return a>b?a:b;
-    }
+
+	private static int result;
+
+
+	public static void main(String[] args) {
+		
+    setResult(maxOfTwoNumbers(2,3));
+		
+	}
+	
+	
+	public static int maxOfTwoNumbers(int a, int b) {
+		
+		if (a > b) {
+			return a;
+		}
+		
+		else {
+			return b;
+		}
+		
+	}
+
+
+	public static int getResult() {
+		return result;
+	}
+
+
+	public static void setResult(int result) {
+		MaxOfTwo.result = result;
+	}
+
 }
--- java-algorithms-challenge/src/test/java/fr/afrogeek/algorithms/MaxOfTwoTest.java
+++ java-algorithms-challenge/src/test/java/fr/afrogeek/algorithms/MaxOfTwoTest.java
@@ -4,26 +4,29 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class MaxOfTwoTest {
+	
 
-    @Test
-    public void testPositiveNumbers() {
-        assertEquals(5, MaxOfTwo.maxOfTwoNumbers(3, 5));
-    }
 
-    @Test
-    public void testNegativeNumbers() {
-        assertEquals(-2, MaxOfTwo.maxOfTwoNumbers(-5, -2));
-    }
+	    @Test
+	    public void positiveTest() {
+	        assertEquals(3, MaxOfTwo.maxOfTwoNumbers(2, 3));
+	    }
 
-    @Test
-    public void testMixedNumbers() {
-        assertEquals(4, MaxOfTwo.maxOfTwoNumbers(-2, 4));
-    }
+	    @Test
+	    public void equalTest() {
+	        assertEquals(2, MaxOfTwo.maxOfTwoNumbers(2, 2));
+	    }
 
-    @Test
-    public void testEqualNumbers() {
-        assertEquals(10, MaxOfTwo.maxOfTwoNumbers(10, 10));
-    }
+	    @Test
+	    public void NegativeTest() {
+	        assertEquals(-2, MaxOfTwo.maxOfTwoNumbers(-2, -3));
+	    }
+
+	    @Test
+	    public void PositiveAndNegativeTest() {
+	        assertEquals(3, MaxOfTwo.maxOfTwoNumbers(-2, 3));
+	    }
 
 }
